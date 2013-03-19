9f0c9a21a3eca74867c9ee06af3234c218899cfb
Jan E <jeebjp@gmail.com>
2013-03-19 17:13:15 +0200

    Add the forums models

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Subforum(models.Model):
+    # if parent == NULL, this is a root subforum
+    parent = models.ForeignKey(Subforum)
+    # content below
+    title  = models.CharField(max_length=200)
+
+class Post(models.Model):
+    # subforum in which this post is set
+    subforum = models.ForeignKey(Subforum)
+    # parent post, if NULL this is a 'thread'
+    parent   = models.ForeignKey(Post)
+    # content below
+    title    = models.CharField(max_length=200)
+    content  = models.TextField()
+    pub_date = models.DateTimeField('date published')
