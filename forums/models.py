a528459cc9d19cd81e6fb02c4c94a1d7602b5adc
Jan E <jeebjp@gmail.com>
2013-03-19 17:38:38 +0200

    Add some model info

--- forums/models.py
+++ forums/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Subforum(models.Model):
-    # if parent == NULL, this is a root subforum
-    parent = models.ForeignKey(Subforum)
+    # if parent == null, this is a root subforum
+    parent = models.ForeignKey('self', null=True)
     # content below
     title  = models.CharField(max_length=200)
 
 class Post(models.Model):
     # subforum in which this post is set
     subforum = models.ForeignKey(Subforum)
-    # parent post, if NULL this is a 'thread'
-    parent   = models.ForeignKey(Post)
+    # parent post, if null, this is a 'thread'
+    parent   = models.ForeignKey('self', null=True)
     # content below
     title    = models.CharField(max_length=200)
     content  = models.TextField()
