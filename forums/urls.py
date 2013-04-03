c0882bd73701e27414cb39fa5747f5dcf9163135
Jan E <jeebjp@gmail.com>
2013-04-03 19:02:12 +0300

    Add more views

--- forums/urls.py
+++ forums/urls.py
@@ -3,5 +3,14 @@ from django.conf.urls import patterns, url
 from forums import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index')
+	# ex: /forums/
+	url(r'^$', views.index, name='index'),
+	# ex: /forums/thread/5
+	url(r'^thread/(?P<thread_id>\d+)$', views.show_thread, name='show_thread'),
+	# ex: /forums/post/5
+	url(r'^post/(?P<post_id>\d+)$', views.show_post, name='show_post'),
+	# ex: /forums/post/add
+	url(r'^post/add$', views.add_post, name='add_post'),
+	# ex: /forums/post/5/edit
+	url(r'^post/(?P<post_id>\d+)/edit$', views.edit_post, name='edit_post'),
 )
--- forums/views.py
+++ forums/views.py
@@ -2,5 +2,22 @@
 
 from django.http import HttpResponse
 
+# General index
 def index(request):
 	return HttpResponse("Hello, world. Saatana.")
+
+# Shows contents of a single thread
+def show_thread(request, thread_id):
+	return HttpResponse("You're looking at thread %s." % thread_id)
+
+# Shows contents of a single post
+def show_post(request, post_id):
+	return HttpResponse("You're looking at post %s." % post_id)
+
+# Lets you add a new post
+def add_post(request):
+	return HttpResponse("You're trying to add a new post.")
+
+# Lets you edit a post
+def edit_post(request, post_id):
+	return HttpResponse("You're trying to edit post %s." % post_id)
