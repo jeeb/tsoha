df670ba236e875579216c520c90935d7c164821c
Jan E <jeebjp@gmail.com>
2013-04-03 18:18:40 +0300

    Enable admin and add administrative functionality to model

---
+++ forums/admin.py
@@ -0,0 +1,14 @@
+# Add models to administration screen
+
+from django.contrib import admin
+from forums.models import Subforum
+from forums.models import Post
+
+class SubforumAdmin(admin.ModelAdmin):
+	list_display = ('title', 'is_root_cat', 'parent')
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'is_op', 'subforum', 'content', 'pub_date')
+
+admin.site.register(Subforum, SubforumAdmin)
+admin.site.register(Post, PostAdmin)
--- forums/models.py
+++ forums/models.py
@@ -4,16 +4,24 @@ from django.db import models
 
 class Subforum(models.Model):
     # if parent == null, this is a root subforum
-    parent = models.ForeignKey('self', null=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
     # content below
     title  = models.CharField(max_length=200)
+    def is_root_cat(self):
+        return (self.parent is None)
+    def __unicode__(self):
+        return self.title
 
 class Post(models.Model):
     # subforum in which this post is set
     subforum = models.ForeignKey(Subforum)
     # parent post, if null, this is a 'thread'
-    parent   = models.ForeignKey('self', null=True)
+    parent   = models.ForeignKey('self', null=True, blank=True)
     # content below
     title    = models.CharField(max_length=200)
     content  = models.TextField()
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date posted')
+    def is_op(self):
+        return (self.parent is None)
+    def __unicode__(self):
+        return self.content
