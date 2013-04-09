e789dc19e811c7602ef1403eae9e7fe3c7cc4b23
Jan E <jeebjp@gmail.com>
2013-04-09 15:31:16 +0300

    ShowForum: Port to Django templates

--- forums/views.py
+++ forums/views.py
@@ -8,10 +8,14 @@ from forums.models import Post, Subforum
 
 # General index
 def index(request):
+    # Grab all root forums
     forum_list = Subforum.objects.filter(parent=None)
 
+    # Load template and set context
     template = loader.get_template('forums/index.html')
     context  = {'forum_list': forum_list}
+
+    # Render the view
     return render(request, 'forums/index.html', context)
 
 # Shows contents of a (sub)forum
@@ -19,24 +23,21 @@ def show_forum(request, forum_id):
     # First try finding the (sub)forum
     forum = get_object_or_404(Subforum, pk=forum_id)
 
-    # Start the list of the threads / subforums
-    thread_list = "<ul>"
-
     # Grab all subforums that are children of this forum
-    for sf in Subforum.objects.filter(parent=forum_id):
-        thread_list += "<li>" + sf.title + "</li>" # Add them to the list
+    subforums = Subforum.objects.filter(parent=forum_id)
+    # Grab all threads of the current (sub)forum in a newer-first order
+    threads   = Post.objects.filter(subforum=forum_id).filter(parent=None).order_by('-pub_date')
 
-    # Grab all threads of the subforum in a newer-first order
-    for t in Post.objects.filter(subforum=forum_id).filter(parent=None).order_by('-pub_date'):
-        thread_list += "<li>" + t.title + "</li>" # Add them to the list
-    # End the list of the threads
-    thread_list += "</ul>"
+    # Load template and set context
+    template = loader.get_template('forums/show_forum.html')
+    context  = {
+        'forum':     forum,
+        'subforums': subforums,
+        'threads':   threads,
+    }
 
-    # Render test page
-    if forum.is_root_cat():
-        return HttpResponse("<h1>%s</h1><p>%s</p>" % (forum.title, thread_list))
-    else:
-        return HttpResponse("<h1>%s :: %s</h1><p>%s</p>" % (forum.parent.title, forum.title, thread_list))
+    # Render the view
+    return render(request, 'forums/show_forum.html', context)
 
 # Shows contents of a single thread
 def show_thread(request, thread_id):
