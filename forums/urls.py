c5c73a0972a443d5a9773920ab299ec4532f9b9b
Jan E <jeebjp@gmail.com>
2013-04-12 21:32:22 +0300

    Urls: Let some pages' urls end with / as well

--- forums/urls.py
+++ forums/urls.py
@@ -6,19 +6,19 @@ urlpatterns = patterns('',
     # ex: /forums/
     url(r'^$', views.index, name='index'),
     # ex: /login
-    url(r'^login$', views.log_in, name='log_in'),
+    url(r'^login/?$', views.log_in, name='log_in'),
     # ex: /logout
-    url(r'^logout$', views.log_out, name='log_out'),
+    url(r'^logout/?$', views.log_out, name='log_out'),
     # ex: /forums/forum/5
-    url(r'^forum/(?P<forum_id>\d+)$', views.show_forum, name='show_forum'),
+    url(r'^forum/(?P<forum_id>\d+)/?$', views.show_forum, name='show_forum'),
     # ex: /forums/forum/5/add
     url(r'^forum/(?P<forum_id>\d+)/add$', views.add_thread, name='add_thread'),
     # ex: /forums/thread/5
-    url(r'^thread/(?P<thread_id>\d+)$', views.show_thread, name='show_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/?$', views.show_thread, name='show_thread'),
     # ex: /forums/thread/5/add
     url(r'^thread/(?P<thread_id>\d+)/add$', views.add_post, name='add_post'),
     # ex: /forums/post/5
-    url(r'^post/(?P<post_id>\d+)$', views.show_post, name='show_post'),
+    url(r'^post/(?P<post_id>\d+)/?$', views.show_post, name='show_post'),
     # ex: /forums/post/5/edit
     url(r'^post/(?P<post_id>\d+)/edit$', views.edit_post, name='edit_post'),
 )
